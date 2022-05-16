a93fdd99a475412c53ecb04e9feebb4ffc56cb9c
Roman Sokolenko <sokolero@yandex.ru>
2022-05-16 22:47:03 +0300

    Add src/cli.js

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from "readline-sync";
+
+export default function main() {
+  const answer = readlineSync.question(
+    `Welcome to the Brain Games! \nMay I have your name?
+    `);
+
+  console.log(`Hello, ${answer}`);
+}
