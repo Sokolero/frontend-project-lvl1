025ff505f83561c6d6cefea0b4726170cafabba9
Roman Sokolenko <sokolero@yandex.ru>
2022-05-17 14:20:44 +0300

    Add Linter Check workflow, update .eslintrc

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,10 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 export default function main() {
   const answer = readlineSync.question(
     `Welcome to the Brain Games! \nMay I have your name?
-    `);
+    `,
+  );
 
   console.log(`Hello, ${answer}`);
 }
