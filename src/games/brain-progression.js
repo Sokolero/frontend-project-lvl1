ec1e9cf344588fd75761eb2025b935fee275ba22
Roman Sokolenko <sokolero@yandex.ru>
2022-05-20 11:35:55 +0300

    Fix linter errors

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -3,7 +3,6 @@ import phrases from '../phrases.js';
 import config from '../app.config.js';
 import getRandomInt from '../utils/index.js';
 
-
 export class GameProgression extends Game {
   static getQuestionText() {
     // return string type of "5 7 9 11 13 .. 17 19"
@@ -22,7 +21,7 @@ export class GameProgression extends Game {
 
     progression[hiddenIndex] = '..';
 
-    return progression.map(item => String(item)).join(' ');
+    return progression.map((item) => String(item)).join(' ');
   }
 
   static getCorrectAnswer(questionText) {
@@ -30,20 +29,20 @@ export class GameProgression extends Game {
     const len = progression.length;
     const hiddenIndex = progression.indexOf('..');
 
-    let step = undefined;
     // если  индекс первый
     if (hiddenIndex === 0) {
       return String(2 * Number(progression[1]) - Number(progression[2]));
     }
     if (hiddenIndex === len - 1) {
-      return String(2 * Number(progression[len - 2]) - Number(progression[len - 3]))
+      return String(2 * Number(progression[len - 2]) - Number(progression[len - 3]));
     }
 
-    return String(Number(progression[hiddenIndex - 1]) + (Number(progression[len - 1]) - Number(progression[0])) / (len - 1))
-
+    return String(
+      Number(progression[hiddenIndex - 1])
+      + (Number(progression[len - 1]) - Number(progression[0])) / (len - 1),
+    );
   }
 }
-
 
 export default function main() {
   const game = new GameProgression(config.ROUNDS, phrases, 'What number is missing in the progression?');
